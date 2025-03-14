49d4096cf8c8b579b79978467bbaeaf3ae400180
pratham-2004 <ps.pratham04@gmail.com>
2025-03-14 14:03:47 +0530

    UART RECEIVE

--- STM32NUCLEOF446RE/UART_RECEIVE/Core/Src/main.c
+++ STM32NUCLEOF446RE/UART_RECEIVE/Core/Src/main.c
@@ -56,6 +56,7 @@ static void MX_USART2_UART_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 uint8_t rx_buffer[4];
+uint8_t rx_buffer2[4];
 /* USER CODE END 0 */
 
 /**
